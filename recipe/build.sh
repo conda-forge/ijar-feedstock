182401fda1254d2b0418c617ae5f447a0eff245f
Uwe L. Korn <uwe.korn@quantco.com>
2024-06-27 10:17:11 +0200

    Use new-style toolchains

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,19 @@ set -euxo pipefail
 
 source gen-bazel-toolchain
 
-sed -i '/incompatible_enable_cc_toolchain_resolution/d' .bazelrc
-
 chmod +x bazel
 pushd third_party/ijar
-../../bazel build --logging=6 --subcommands --verbose_failures --crosstool_top=//bazel_toolchain:toolchain --cpu ${TARGET_CPU} zipper ijar
+../../bazel build \
+	--logging=6 \
+	--subcommands \
+	--verbose_failures \
+	--local_resources=cpu=1 \
+	--extra_toolchains=//bazel_toolchain:cc_cf_toolchain \
+	--extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain \
+	--platforms=//bazel_toolchain:target_platform \
+	--host_platform=//bazel_toolchain:build_platform \
+	--cpu=${TARGET_CPU} \
+	zipper ijar
 mkdir -p $PREFIX/bin
 cp ../../bazel-out/${TARGET_CPU}-fastbuild/bin/third_party/ijar/ijar $PREFIX/bin
 cp ../../bazel-out/${TARGET_CPU}-fastbuild/bin/third_party/ijar/zipper $PREFIX/bin
